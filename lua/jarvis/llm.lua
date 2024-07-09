52c7578622ca4a7f85978366e2b980e5b79c4362
Mathieu Tuli <tuli.mathieu@gmail.com>
2024-07-08 22:47:02 -0400

    Update default model handler

--- lua/jarvis/llm.lua
+++ lua/jarvis/llm.lua
@@ -2,9 +2,41 @@ local _G = {}
 local Job = require("plenary.job")
 local Utils = require("jarvis.utils")
 
+-- DEFEAULT BEHAVIOUR
+local model_name = "gpt-4o"
+local url = "https://api.openai.com/v1/chat/completions"
+local api_key_name = "OPENAI_API_KEY"
+local system_prompt = "You are my helpful assistant coder. Try to be as non-verbose as possible and stick to the important things. Avoid describing your own code unnecessarily, I only want you to output code mainly and limit describing it."
+
+local function openai_data_handler(data_stream)
+    if data_stream:match '"delta":' then
+        local json = vim.json.decode(data_stream)
+        if json.choices and json.choices[1] and json.choices[1].delta then
+            return json.choices[1].delta.content
+        end
+    end
+end
+
+local function make_openai_curl_args(history, prompt)
+    local api_key = os.getenv(api_key_name)
+    local data = {
+        messages = { { role = 'system', content = system_prompt }, { role = 'user', content = history .. prompt } },
+        model = model_name,
+        temperature = 0.7,
+        stream = true,
+    }
+    local args = { '-N', '-X', 'POST', '-H', 'Content-Type: application/json', '-d', vim.json.encode(data) }
+    if api_key then
+        table.insert(args, '-H')
+        table.insert(args, 'Authorization: Bearer ' .. api_key)
+    end
+    table.insert(args, url)
+    return args
+end
+_G.data_handler = openai_data_handler
+_G.make_curl_args = make_openai_curl_args
+--
 local active_job = nil
-_G.data_handler = function(data_stream) error("Undefined function. See README for setup.") end
-_G.make_curl_args = function(history, prompt) error("Undefined function. See README for setup.") end
 function _G.get_response_and_stream_to_buffer(history_winid, history_bufnr, prompt_winid, prompt_bufnr)
     local history_lines = vim.api.nvim_buf_get_lines(history_bufnr, 0, -1, false)
     local prompt_lines = vim.api.nvim_buf_get_lines(prompt_bufnr, 0, -1, false)
